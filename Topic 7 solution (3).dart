e30a7a721ac10b2ab2676636082f7e3dc38a3ad5
bipro0182410012101113 <biprodas050@gmail.com>
2026-03-18 14:35:56 +0600

    Update Topic 7 solution (3).dart

---
+++ Topic 7 solution (3).dart
@@ -0,0 +1,12 @@
+void main() {
+  int? age;         
+  String? name;     
+  bool? isActive;   
+  age = null;
+  name = "Bipro";
+  isActive = true;
+
+  print(age);       
+  print(name);      
+  print(isActive);  
+}
